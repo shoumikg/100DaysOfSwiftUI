145d91268c46c4207701d419c440a1fe7e53add4
Shoumik Ghosal <shoumikghosal@airasia.com>
2024-01-09 11:40:48 +0530

    Initial commit

---
+++ Checkpoint 1/UnitConverterApp/UnitConverterApp/UnitConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterAppApp.swift
+//  UnitConverterApp
+//
+//  Created by Shoumik on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
