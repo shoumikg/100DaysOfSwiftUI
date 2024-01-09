51f17acf217397508b884879f0f34b9f14238de8
Shoumik Ghosal <shoumikghosal@airasia.com>
2024-01-09 12:44:02 +0530

    Static converter

--- Checkpoint 1/UnitConverterApp/UnitConverterApp/ContentView.swift
+++ Checkpoint 1/UnitConverterApp/UnitConverterApp/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var fahrenheitValue = (32.5)
+    var celciusValue: Double {
+        return (fahrenheitValue - 32) * (5/9)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack (alignment: .leading,
+                spacing: 30) {
+            Text("Temperature")
+            HStack (spacing: 20) {
+                UnitView(value: fahrenheitValue, title: "Fahrenheit")
+                Text("=")
+                UnitView(value: celciusValue, title: "Celcius")
+            }
+            HStack (spacing: 20) {
+                Text("Formula")
+                    .padding(5)
+                    .background(Color.yellow)
+                Text(String(format: "(%.2f°F − 32) × 5/9 = %.2f°C",
+                            fahrenheitValue,
+                            celciusValue))
+            }
+            .font(.subheadline)
+            Spacer()
         }
-        .padding()
+        .frame(width: 350,
+               height: 300,
+               alignment: .leading)
+        .font(.title)
+        .scaledToFill()
+    }
+    
+    struct UnitView: View {
+        var value: Double
+        var title: String
+        
+        var body: some View {
+            VStack {
+                Text(String(format: "%.2f",
+                            value))
+                    .font(.title2)
+                Text(title)
+                    .font(.title3)
+            }
+        }
     }
 }
 
